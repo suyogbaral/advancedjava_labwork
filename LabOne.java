553ae038a20690a78dcf7c6fd269d31069057c52
Suyog Baral <37suyogbrl@gmail.com>
2025-11-19 18:41:49 +0545

    First Commit

---
+++ LabOne.java
@@ -0,0 +1,8 @@
+public class LabOne {
+        public static void main(String[] args) {
+        int n = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", n, i, n * i);
+        }
+    }
+}
