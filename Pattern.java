fce521d022f0f07c553d5fdb60fd14177330b14d
Suyog Baral <37suyogbrl@gmail.com>
2025-11-28 20:31:49 +0545

    Priniting the pattern

---
+++ Pattern.java
@@ -0,0 +1,11 @@
+class Pattern{
+    public static void main(String[] args) {
+        int a=4;
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=i;j++){
+                System.out.printf("%d", j);
+            }
+            System.out.printf("%n");
+        }
+    }
+}
